3ee368f5dc297f226ce298934d86dc269b195113
githussein <mo.samy.hussein@gmail.com>
2021-01-05 19:28:05 +0100

    Home Screen Header

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:corona_stats/config/palette.dart';
+import 'package:corona_stats/config/styles.dart';
+import 'package:corona_stats/widgets/dart/country_dropdown.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:corona_stats/widgets/dart/custom_app_bar.dart';
 
@@ -8,6 +11,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String _country = 'DE';
+
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
@@ -29,7 +34,7 @@ class _HomeScreenState extends State<HomeScreen> {
         decoration: BoxDecoration(
           color: Palette.primaryColor,
           borderRadius: BorderRadius.only(
-            // bottomRight: Radius.circular(0.0),
+            bottomRight: Radius.circular(40.0),
             bottomLeft: Radius.circular(40.0),
           ),
         ),
@@ -37,14 +42,88 @@ class _HomeScreenState extends State<HomeScreen> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 Text(
                   'COVID-19',
                   style: const TextStyle(
                     color: Colors.white,
-                    fontSize: 25.0,
+                    fontSize: 32.0,
                     fontWeight: FontWeight.bold,
                   ),
+                ),
+                CountryDropdown(
+                  countries: [
+                    'DE',
+                    'EG',
+                    'AU',
+                    'IT',
+                    'US',
+                    'RU',
+                  ],
+                  country: _country,
+                  onChanged: (val) => setState(() => _country = val),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: screenHeight * 0.03,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Are you feeling sick?',
+                  style: const TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600),
+                ),
+                SizedBox(height: screenHeight * 0.01),
+                Text(
+                  'If you are having any of COVID-19 symptoms please contact us.',
+                  style: const TextStyle(
+                    fontSize: 14.0,
+                    color: Colors.white70,
+                  ),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.03,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    FlatButton.icon(
+                        onPressed: () {},
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 20.0),
+                        color: Colors.green,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0)),
+                        icon: const Icon(
+                          Icons.phone,
+                          color: Colors.white,
+                        ),
+                        label: Text(
+                          'Call now',
+                          style: Styles.buttonTextStyle,
+                        )),
+                    FlatButton.icon(
+                        onPressed: () {},
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 20.0),
+                        color: Colors.blue,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0)),
+                        icon: const Icon(
+                          Icons.message,
+                          color: Colors.white,
+                        ),
+                        label: Text(
+                          'Send SMS',
+                          style: Styles.buttonTextStyle,
+                        )),
+                  ],
                 )
               ],
             )
