4eac50744c7ccf840d66f8e79e93c2392e9af887
githussein <mo.samy.hussein@gmail.com>
2021-01-05 13:12:02 +0100

    Bottom Navigation Bar

---
+++ android/app/src/main/kotlin/com/mhussein/corona_stats/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mhussein.corona_stats
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/data.dart
@@ -0,0 +1,5 @@
+final prevention = [
+  {'assets/images/de_flag.jpg': 'Avoid close\ncontact'},
+];
+
+final covidDeDailyNewCases = [12.17, 11.15, 10.02, 11.21, 13.83, 14.16, 14.30];
